279eab1628889037fec9ed7384b9323570b3f4d6
sujalamatya <sujalcr10@gmail.com>
2025-02-23 14:49:55 +0545

    feat: add landing page with trending api

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -43,16 +43,40 @@ export default function Home() {
 
   return (
     <div className="flex flex-col justify-center items-center bg-black">
-      {loading && <p className="text-3xl">Loading movies...</p>}
       {error && <p className="text-red-500">{error}</p>}
-      <h1 className="flex justify-center text-3xl font-bold p-5 mt-8">
-        Trending This Week
+      <h1 className="flex justify-center text-3xl font-bold p-5 mt-8 text-amber-600">
+        Trending Movies This Week
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3 mt-4">
+      {loading && (
+        <div className=" flex text-3xl h-[85dvh] items-center text-white">
+          <svg
+            className="mr-3 -ml-1 size-5 animate-spin text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Loading movies...
+        </div>
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-y-6 gap-x-6 mt-4">
         {movies.map((movie: any) => (
           <Card
             key={movie.id}
-            className="w-80 shadow-lg relative overflow-hidden"
+            className="w-80 shadow-lg relative overflow-hidden bg-black border-amber-600/50 hover:border-amber-600"
           >
             <CardContent className="relative">
               <div className="relative">
@@ -63,7 +87,7 @@ export default function Home() {
                 />
                 <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-300">
                   <div className="flex flex-col justify-center items-center h-full text-white">
-                    <h3 className="text-xl font-bold">
+                    <h3 className="text-xl font-bold text-amber-600">
                       {movie.title || movie.name}
                     </h3>
                     <p className="mt-2 text-sm px-4">
@@ -75,7 +99,10 @@ export default function Home() {
             </CardContent>
             <CardFooter>
               <div className="flex justify-between w-full">
-                <Button variant={"ghost"}>
+                <Button
+                  variant={"ghost"}
+                  className="text-amber-600 hover:text-amber-500 hover:bg-black"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -91,7 +118,10 @@ export default function Home() {
                     />
                   </svg>
                 </Button>
-                <Button variant={"ghost"}>
+                <Button
+                  variant={"ghost"}
+                  className="text-amber-600 hover:text-amber-500 hover:bg-black"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
