72ba166d91fb060ca8917346bb67aa6a9d310c69
sujalamatya <sujalcr10@gmail.com>
2025-02-23 14:59:55 +0545

    feat(navbar):add search icon

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -5,19 +5,44 @@ const Navbar: React.FC = () => {
     <nav className="bg-black text-white py-4 px-6 flex justify-between items-center border-b-2 border-gray-600">
       <div className="text-xl font-bold">MoveIT</div>
 
-      {/* Nav Links */}
-      <ul className="flex space-x-6">
-        {["Home", "MyMovies", "Favorites"].map((item) => (
-          <li key={item}>
-            <Link
-              href={item === "Home" ? "/" : `/${item.toLowerCase()}`}
-              className="relative hover:underline hover:underline-offset-4"
-            >
-              {item}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {/* Container for the search form and nav links */}
+      <div className="flex items-center space-x-6">
+        {/* Search Form */}
+        <form action="" className="relative w-max">
+          <input
+            type="search"
+            className="peer cursor-pointer relative z-10 h-12 w-12 rounded-full border bg-transparent pl-12 outline-none focus:w-full focus:cursor-text focus:border-white focus:pl-16 focus:pr-4"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute inset-y-0 my-auto h-8 w-12 border-r border-transparent stroke-white px-3.5 peer-focus:border-white peer-focus:stroke-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </form>
+
+        {/* Navigation Links */}
+        <ul className="flex space-x-6">
+          {["Home", "MyMovies", "Favorites"].map((item) => (
+            <li key={item}>
+              <Link
+                href={item === "Home" ? "/" : `/${item.toLowerCase()}`}
+                className="relative hover:underline hover:underline-offset-4"
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
