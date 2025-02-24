6b0bb3bc01d6e6be26573f100713caa0f1c6570a
sujalamatya <sujalcr10@gmail.com>
2025-02-24 09:46:53 +0545

    feat: add favorite feature in watched movies list

--- src/app/mymovies/page.tsx
+++ src/app/mymovies/page.tsx
@@ -9,10 +9,12 @@ export default function Page() {
   const {
     watchlist,
     watched,
+    favorite,
     setWatchlist,
     setWatched,
     removeFromWatchlist,
     removeFromWatched,
+    setFavorite,
   } = useStore();
 
   const handleDragStart = (event: React.DragEvent, movie: any) => {
@@ -28,6 +30,10 @@ export default function Page() {
 
   const allowDrop = (event: React.DragEvent) => {
     event.preventDefault();
+  };
+
+  const isFavorite = (movieId: number) => {
+    return favorite.some((m) => m.id === movieId);
   };
 
   return (
@@ -71,7 +77,20 @@ export default function Page() {
                     className="text-amber-600 hover:text-amber-800 hover:bg-black"
                     onClick={() => removeFromWatchlist(movie.id)}
                   >
-                    Remove
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
                   </Button>
                 </CardFooter>
               </Card>
@@ -117,13 +136,46 @@ export default function Page() {
                     </div>
                   </div>
                 </CardContent>
-                <CardFooter className="flex justify-center">
+                <CardFooter className="flex justify-between">
                   <Button
                     variant={"ghost"}
                     className="text-amber-600 hover:text-amber-800 hover:bg-black"
                     onClick={() => removeFromWatched(movie.id)}
                   >
-                    Remove
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                      />
+                    </svg>
+                  </Button>
+                  <Button
+                    variant={"ghost"}
+                    className="text-amber-600 hover:text-amber-800 hover:bg-black"
+                    onClick={() => setFavorite(movie)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill={isFavorite(movie.id) ? "currentColor" : "none"}
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                      />
+                    </svg>
                   </Button>
                 </CardFooter>
               </Card>
